e81689cabb5184e18ec1ee421bf26fd00ae081fd
임준서 <treetowood14@gmail.com>
2026-07-27 01:23:38 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,7 @@ const API_KEY = "ee14d076297bf7cdbbdfb9e7e3562bbf";
 function geoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
+    //I tried to connect api, but it was not working. Also I couldn't do anything because it was 1:20 am. If I wake up early and if I can fix it, I will tired to fix it.
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(response => response.json())
@@ -12,6 +13,7 @@ function geoSuccess(position) {
             city.innerText = data.name;
             weather.innerText = data.weather[0].main;
         });
+    console.log(lat, lon);
 }
 
 function geoError() {
